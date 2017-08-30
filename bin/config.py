a989e23e98db54bde3b222cd9f72a252d0211803
yongjiel <yongjiel@ualberta.ca>
2017-08-30 17:52:23 +0000

    Update source code and README. And more checking mechanism to run_jobs_each_host.py

--- bin/config.py
+++ bin/config.py
@@ -5,3 +5,4 @@ pieces_in_each_host = 2 #split input file into these pieces in each host
 out_dir = "/home/centos/cfm_id/cfmid/output"  # dir where to store the output of docker image
 case_dir = ["positive", "negative", "ei"] # the specific case output dir for each image run
 dir_back_to_host = "/mnt/one/results"  # dir of the result file back to master host
+timeout = 40 # minutes for time out of docker run
--- bin/run_job_each_host.py
+++ bin/run_job_each_host.py
@@ -12,15 +12,41 @@ from util import get_input_file, split_input_file
 import re
 import subprocess
 from multiprocessing import Pool
-
+import time
 
 def jobs(file):
+    done_file = file + ".done"
+    done_list = []
+    if os.path.isfile(done_file):
+        with open(done_file, 'r') as df:
+            done_list = df.read().split("\n")
     with open(file, "r") as f:
         for line in f:
             m = re.search("^(\S*)\t(\S*)", line)
             hmdb_id = m.group(1)
+            if hmdb_id in done_list:
+                continue
             smile = m.group(2)
             for c in config.case_dir:
+                remote_file = "{0}/{1}/{2}*".format(config.dir_back_to_host, c, hmdb_id)
+                cm1 = "ssh centos_1 \"ls {0}\"".format(remote_file)
+                count = 0
+                read_out = ''
+                while(1):
+                    count += 1
+                    rp = subprocess.Popen(cm1, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+                    rp.wait()
+                    read_out = rp.stdout.read()
+                    if read_out:
+                        break
+                    if count == 3:
+                        break
+                    time.sleep(3)
+
+                if read_out:
+                    #print >> sys.stderr, "Bypass " + remote_file
+                    continue
+                #print >> sys.stderr, "Run " + remote_file
                 if c == 'positive':
                     param_file = "/root/param_output0.log"
                     config_file = "/root/param_config.txt"
@@ -36,9 +62,17 @@ def jobs(file):
                      "1 /root/{4}/{5}; chmod 777 /root/{4}/{5}\" ").format(config.out_dir, smile, \
                                             param_file, config_file, c, \
                                             "{0}.out".format(hmdb_id))
+                x = config.timeout * 60 
+                delay = 3
+                timeout = int(x / delay)
                 p = subprocess.Popen(cmd, stdout=subprocess.PIPE, \
                         stderr=subprocess.STDOUT, shell=True)
-                p.wait()
+                while p.poll() is None and timeout > 0:
+                    time.sleep(delay)
+                    timeout -= delay
+                #p.wait()
+                if timeout == 0:
+                    p.kill()
                 output_file = "{0}/{1}/{2}".format(config.out_dir, c, "{0}.out".format(hmdb_id))
                 file_to_master_host = ''
                 # if failed, generate fail file
@@ -46,7 +80,10 @@ def jobs(file):
                     fail_file = "{0}/{1}/{2}".format(config.out_dir, c, "{0}.fail".format(hmdb_id))
                     with open(fail_file, "w") as ff:
                         ff.write(cmd + "\n")
-                        ff.write(p.stdout.read() + "\n")
+                        if timeout == 0:
+                            ff.write("Time out {0} minutes!\n".format(config.timeout))
+                        else:
+                            ff.write(p.stdout.read() + "\n")
                     file_to_master_host = fail_file
                 else:
                     file_to_master_host = output_file
@@ -57,7 +94,9 @@ def jobs(file):
                 p.wait()
                 # delete the result file
                 os.remove(file_to_master_host)
-
+            #write done file
+            with open(done_file, "a") as df:
+                df.write(hmdb_id + "\n")
 
 def run_jobs(input_file, pieces):
     argvs = []
@@ -65,6 +104,7 @@ def run_jobs(input_file, pieces):
         file = "{0}_{1}".format(input_file, i)
         argvs.append(file)
     p = Pool(pieces)
+    print argvs
     p.map(jobs, argvs)
     p.close()
     p.join()
@@ -72,6 +112,7 @@ def run_jobs(input_file, pieces):
 def main():
     input_file = get_input_file("run_job_each_host.py")
     pieces = split_input_file(input_file, config.pieces_in_each_host)
+    print "Pieces {0}".format(pieces)
     run_jobs(input_file, pieces)
     print "Program exit!"
 
